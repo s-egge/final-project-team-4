d52d48296f151053652718b6ca556cff8aa13780
s-egge <102624422+s-egge@users.noreply.github.com>
2022-05-17 18:31:08 -0700

    Highscore modal added
    -Added high score modal with open/close functionality
    -Added and styled table for high scores
    -Renamed modal classes to reduce duplication of code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var startButton = document.getElementById('start-button')
-var closeGuideButton = document.querySelector('.close-guide-button')
+var closeModalButton = document.querySelectorAll('.close-modal-button')
 var modalBackdrop = document.getElementById('modal-backdrop')
 var guideButton = document.getElementById('guide-button')
 var guideModal = document.getElementById('guide-modal')
+var highScoreButton = document.getElementById('scores-button')
+var highScoreModal = document.getElementById('high-score-modal')
 
 startButton.addEventListener('click', function(){
     window.location.href='./gameplay.html'
@@ -13,7 +15,16 @@ guideButton.addEventListener('click', function(){
     guideModal.classList.remove('hidden')
 })
 
-closeGuideButton.addEventListener('click', function(){
-    modalBackdrop.classList.add('hidden')
-    guideModal.classList.add('hidden')
+highScoreButton.addEventListener('click', function(){
+    modalBackdrop.classList.remove('hidden')
+    highScoreModal.classList.remove('hidden')
+    console.log(highScoreModal)
 })
+
+//closes both the guide and high-score modal
+for(var i = 0; i < closeModalButton.length; i++){
+    closeModalButton[i].addEventListener('click', function(event){
+        event.target.parentNode.parentNode.classList.add('hidden')
+        modalBackdrop.classList.add('hidden')
+    })
+}
