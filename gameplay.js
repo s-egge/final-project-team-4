ea77b85e7f3c46e66a5dfa3a73be36873bd9c9a5
s-egge <102624422+s-egge@users.noreply.github.com>
2022-05-20 11:32:23 -0700

    pause-play styling
    - added icons in place of play/pause button that changes depending on if games is paused or not
    - styled "game is paused" text and score/health bars

--- gameplay.js
+++ gameplay.js
@@ -110,9 +110,11 @@ function toggle_game_paused() {
     if (game_paused) {
         game_paused = false
         document.getElementById('game_paused_screen').classList.add('hidden')
+        document.querySelector(".play-pause-img").src = "./images/pause.png"
     } else {
         game_paused = true
         document.getElementById('game_paused_screen').classList.remove('hidden')
+        document.querySelector(".play-pause-img").src = "./images/play.png"
     }
 }
 
